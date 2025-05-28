2394b7846816a390263f906e070b0708e3deb38b
kubilay <41905346+kubilaiswf@users.noreply.github.com>
2025-05-28 10:17:51 +0300

    fix, duplicate

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,18 @@ export default defineConfig({
   assetsInclude: ['**/*.geojson'],
   server: {
     open: true
+  },
+  build: {
+    cssMinify: 'esbuild',
+    minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+          charts: ['chart.js', 'react-chartjs-2', 'react-simple-maps']
+        }
+      }
+    }
   }
 }); 
